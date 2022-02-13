34066ee729b57f2aeb4dd686f3c237ddd7b0d031
Mamadou NDONGO <ndong003@gmail.com>
2022-02-13 14:30:10 +0100

    index frontend

--- models/user.model.js
+++ models/user.model.js
@@ -1,73 +1,42 @@
-const mongoose = require('mongoose');
+const { Model } = require("sequelize");
 const { isEmail } = require('validator');
 const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema(
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {}
+  User.init({
+      userName: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      email: {
+          type: DataTypes.STRING,
+          unique: true,
+          allowNull: false
+      },
+      password: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      avatar: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          defaultValue: "http://127.0.0.1:3000/images/avatars/default_user.jpg"
+      },
+      isAdmin: {
+          type: DataTypes.BOOLEAN,
+          allowNull: false,
+          defaultValue: false
+      },
+      isActive: {
+          type: DataTypes.BOOLEAN,
+          allowNull: false,
+          defaultValue: true
+      }
+  }, 
   {
-    pseudo: {
-      type: String,
-      required: true,
-      minLength: 3,
-      maxLength: 55,
-      unique: true,
-      trim: true
-    },
-    email: {
-      type: String,
-      required: true,
-      validate: [isEmail],
-      lowercase: true,
-      unique: true,
-      trim: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      max: 1024,
-      minlength: 6
-    },
-    picture: {
-      type: String,
-      default: "./uploads/profil/random-user.png"
-    },
-    bio :{
-      type: String,
-      max: 1024,
-    },
-    followers: {
-      type: [String]
-    },
-    following: {
-      type: [String]
-    },
-    likes: {
-      type: [String]
-    }
-  },
-  {
-    timestamps: true,
-  }
-);
-
-// play function before save into display: 'block',
-userSchema.pre("save", async function(next) {
-  const salt = await bcrypt.genSalt();
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-userSchema.statics.login = async function(email, password) {
-  const user = await this.findOne({ email });
-  if (user) {
-    const auth = await bcrypt.compare(password, user.password);
-    if (auth) {
-      return user;
-    }
-    throw Error('incorrect password');
-  }
-  throw Error('incorrect email')
-};
-
-const UserModel = mongoose.model("user", userSchema);
-
-module.exports = UserModel;
+      sequelize,
+      modelName: "User"
+  })
+  return User
+}
