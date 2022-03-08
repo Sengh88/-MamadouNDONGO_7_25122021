b414ddb3d5a49aa93349b6d01f7e96a571d2d2b3
Mamadou NDONGO <ndong003@gmail.com>
2022-03-08 18:47:46 +0100

    models

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
-
-mongoose
-.connect("mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.fnwdr.mongodb.net/groupomania",
-{
- useNewUrlParser: true,
- useUnifiedTopology: true,
- 
-}
-)
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.log("Failed to connect to MongoDB", err))
+require("dotenv").config();
+module.exports = {
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: process.env.PASSWORD,
+  DB: "groupomania",
+  dialect: "postegresql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
